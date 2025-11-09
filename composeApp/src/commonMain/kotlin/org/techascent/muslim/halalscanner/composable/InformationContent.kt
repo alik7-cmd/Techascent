106f78990ef1ae66ff145ad578c2ef710823138d
Fatema B.Ehsaan <faehs1554@oslomet.no>
2025-11-09 17:54:52 +0100

    add new address model

--- composeApp/src/commonMain/kotlin/org/techascent/muslim/halalscanner/composable/InformationContent.kt
+++ composeApp/src/commonMain/kotlin/org/techascent/muslim/halalscanner/composable/InformationContent.kt
@@ -6,15 +6,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import apphub.composeapp.generated.resources.Res
 import apphub.composeapp.generated.resources.ic_halal
@@ -54,8 +59,11 @@ internal fun InformationContent(
                 ComposeAsyncImage(
                     modifier = Modifier
                         .padding(horizontal = ComposaSpacing.Medium)
-                        .align(Alignment.CenterHorizontally),
+                        .align(Alignment.CenterHorizontally)
+                        .size(120.dp)
+                        .clip(CircleShape),
                     model = it,
+                    contentScale = ContentScale.Crop,
                     contentDescription = productUiState.labels
                 )
             }
