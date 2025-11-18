543582b65e39bf26e2dc0003ebbd927c16093862
Fatema B.Ehsaan <faehs1554@oslomet.no>
2025-11-18 23:19:52 +0100

    update version for release

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         applicationId = "org.techascent.muslim"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
     }
     packaging {
         resources {
