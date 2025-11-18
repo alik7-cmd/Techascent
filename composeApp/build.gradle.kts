7b4c32b44b77cde466c5a476b23a88a061b48e7c
Fatema B.Ehsaan <faehs1554@oslomet.no>
2025-11-18 16:03:08 +0100

    make new release

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         applicationId = "org.techascent.muslim"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
     }
     packaging {
         resources {
