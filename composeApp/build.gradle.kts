1238d0f9e48cf0f89fa49c7b27114e5d0bea879d
rafiquel islam <k.h.rafiquel.islam@dnb.no>
2026-04-11 16:07:04 +0200

    Make a new release (29)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -120,8 +120,8 @@ android {
         applicationId = "org.techascent.muslim"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 28
-        versionName = "2.8.0"
+        versionCode = 29
+        versionName = "2.9.0"
     }
     packaging {
         resources {
